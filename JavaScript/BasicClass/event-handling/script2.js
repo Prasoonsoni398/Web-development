4125334b3cd55a52929ceaea2ca13805e0212a60
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-05-08 18:45:07 +0530

    Refactor JavaScript functions for improved readability and consistency

--- JavaScript/BasicClass/event-handling/script2.js
+++ JavaScript/BasicClass/event-handling/script2.js
@@ -1,32 +1,34 @@
 document.getElementById("bgColor").addEventListener("change", ChangeColor);
 
-function ChangeColor(){
-    const color = document.getElementById("bgColor").value;
-    document.getElementById("changeColor").style.backgroundColor = color;
+function ChangeColor() {
+  const color = document.getElementById("bgColor").value;
+  document.getElementById("changeColor").style.backgroundColor = color;
+  
 }
-document.getElementById("headingColor").addEventListener("change", ChangeColor1);
+document
+  .getElementById("headingColor")
+  .addEventListener("change", ChangeColor1);
 
-function ChangeColor1(){
-    const color = document.getElementById("headingColor").value;
-    document.getElementById("heading").style.color = color;
+function ChangeColor1() {
+  const color = document.getElementById("headingColor").value;
+  document.getElementById("heading").style.color = color;
 }
 document.getElementById("paraColor").addEventListener("change", ChangeColor2);
 
-function ChangeColor2(){
-    const color = document.getElementById("paraColor").value;
-    document.getElementById("para").style.color = color;
+function ChangeColor2() {
+  const color = document.getElementById("paraColor").value;
+  document.getElementById("para").style.color = color;
 }
-
 
 const btn = document.getElementById("reset");
 
-  btn.addEventListener("click", (e) => {
-    const buttonWidth = btn.offsetWidth;
-    const clickPosition = e.offsetX;
+btn.addEventListener("click", (e) => {
+  const buttonWidth = btn.offsetWidth;
+  const clickPosition = e.offsetX;
 
-    if (clickPosition < buttonWidth / 2) {
-      document.getElementById("bg-color1").style.backgroundColor = "blue"
-    } else {
-      window.location.reload();
-    }
-  });
+  if (clickPosition < buttonWidth / 2) {
+    document.getElementById("bg-color1").style.backgroundColor = "blue";
+  } else {
+    window.location.reload();
+  }
+});
