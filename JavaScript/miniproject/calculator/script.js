b999c7c8ad391b640b61ba85f7696316cdf1bcd4
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-05-10 20:25:37 +0530

    Refactor calculator UI and functionality for improved user experience

--- JavaScript/miniproject/calculator/script.js
+++ JavaScript/miniproject/calculator/script.js
@@ -1,4 +1,23 @@
-document.querySelector("button").addEventListener("click", (event) => {
-    console.log(event.target);
-    
-});
+function Calculate(data) {
+  const expression = document.getElementById("text");
+  if (data === "AC") {
+    expression.innerText = "0";
+    return;
+  }
+  if (data === "C") {
+    expression.innerText = expression.innerText.slice(0, -1);
+    return;
+  }
+  if (data === "=") {
+    const Eq = expression.innerText;
+    const solution = eval(Eq);
+    expression.innerText = solution;
+    return;
+  }
+
+  if (expression.innerText === "0") {
+    expression.innerText = data;
+  } else {
+    expression.innerText = expression.innerText + data;
+  }
+}
--- Projects/tip-calculator/script.js
+++ Projects/tip-calculator/script.js
@@ -1,10 +1,1 @@
-function Calculate(data) {
-  const expression = document.getElementById("text");
-  if (data === "AC") {
-    expression.innerText = "0";
-  }
-  if (data === "C") {
-    const eq = expression.innerText;
-    let newEQ = eq.slice(0, -1);
-  }
-}
+
