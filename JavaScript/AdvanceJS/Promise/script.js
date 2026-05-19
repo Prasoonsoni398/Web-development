cb39ae44fff8e9de8f47dd48c7d795fbd0ca46a3
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-05-19 20:01:04 +0530

    Refactor getData function to use async/await syntax and update HTML structure for clarity

--- JavaScript/AdvanceJS/Promise/script.js
+++ JavaScript/AdvanceJS/Promise/script.js
@@ -1,4 +1,4 @@
-function getData(value) {
+async function getData(value) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (value === 3) {
@@ -7,13 +7,18 @@ function getData(value) {
         console.log("Data sent", value);
         resolve("Query Solver");
       }
-    }, 5000);
+    }, 1000);
   });
 }
 
-getData(1)
-  .then(() => getData(2))
-  .then(() => getData(3))
-  .then(() => getData(4))
-  .then(() => getData(5))
-  .catch((rej) => console.log(rej));
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .then(() => getData(5))
+//   .catch((rej) => console.log(rej));
+
+await getData(1)
+await getData(2)
+await getData(3)
+await getData(4)
