1394dd536537db6d5e43eedd1ecd3fb6148d6423
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-05-29 12:00:00 +0530

    Update project

--- Projects/FakeStore/src/App.jsx
+++ Projects/FakeStore/src/App.jsx
@@ -14,7 +14,8 @@ const App = () => {
     const storedTheme = localStorage.getItem('fakestore-theme')
     const defaultTheme = storedTheme || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
     setTheme(defaultTheme)
-  }, []) 
+  }, [])    
+  
 
   useEffect(() => {
     document.documentElement.dataset.theme = theme
