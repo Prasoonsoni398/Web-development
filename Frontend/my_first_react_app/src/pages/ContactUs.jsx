ed7ac6c2c8bc01888ad12bac2e17aa9287c0e85d
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-06 19:59:18 +0530

    feat: enhance ContactUs and Register components with form handling and improved styling

--- Frontend/my_first_react_app/src/pages/ContactUs.jsx
+++ Frontend/my_first_react_app/src/pages/ContactUs.jsx
@@ -1,70 +1,70 @@
 import { useState } from "react";
 
 function ContactUs() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [userName,setUserName] = useState("")
-  const [email,setEmail] = useState("")
-  const [message,setMessage] = useState("")
-
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("UserName :",userName);
-    console.log("Email :",email);
-    console.log("Message :",message);
-    
-  }
+    console.log("UserName :", userName);
+    console.log("Email :", email);
+    console.log("Message :", message);
+  };
 
   return (
-   <>
-    <div className="d-flex justify-content-center">
-      <div className="bg-light border shadow rounded w-50 mt-5 p-5">
-        <form onSubmit={handleSubmit} >
+    <>
+      <div className="d-flex justify-content-center">
+        <div className="bg-light border shadow rounded w-50 mt-5 p-5">
+          <h1 className="text-center fw-bold text-primary">Contact Us</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label fw-semibold">Full Name</label>
 
-          <div className="mb-3">
-                <label className="form-label fw-semibold">Full Name</label>
+              <input
+                type="text"
+                className="form-control shadow-none border-2"
+                placeholder="Enter Your Name"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
 
-                <input
-                  type="text"
-                  className="form-control shadow-none border-2"
-                  placeholder="Enter Your Name"
-                  value={userName}
-                  onChange={(e)=>setUserName(e.target.value)}
-                />
-              </div>
+            <div className="mb-3">
+              <label className="form-label fw-semibold">Email Address</label>
 
-              <div className="mb-3">
-                <label className="form-label fw-semibold">Email Address</label>
+              <input
+                type="email"
+                className="form-control shadow-none border-2"
+                placeholder="Enter Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-                <input
-                  type="email"
-                  className="form-control shadow-none border-2"
-                  placeholder="Enter Your Email"
-                   value={email}
-                  onChange={(e)=>setEmail(e.target.value)}
-                />
-              </div>
+            <div className="mb-3">
+              <label className="form-label fw-semibold">Message</label>
 
-              <div className="mb-3">
-                <label className="form-label fw-semibold">Message</label>
+              <textarea
+                className="form-control shadow-none border-2 "
+                rows={4}
+                style={{ resize: "none" }}
+                placeholder="Enter Your Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
 
-                <textarea
-                  className="form-control shadow-none border-2"
-                  rows="4"
-                  placeholder="Enter Your Message"
-                   value={message}
-                  onChange={(e)=>setMessage(e.target.value)}
-                ></textarea>
-              </div>
-
-             <div className="d-flex justify-content-center mt-3">
-               <button className="btn btn-primary px-4 py-2" type="submit">Send Message</button>
-             </div>
-        </form>
-
+            <div className="d-flex justify-content-center mt-3">
+              <button className="btn btn-primary px-4 py-2" type="submit">
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-   </>
+    </>
   );
 }
 
--- Frontend/my_first_react_app/src/pages/Register.jsx
+++ Frontend/my_first_react_app/src/pages/Register.jsx
@@ -1,60 +1,79 @@
+import { useState } from "react";
+
 function Register() {
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPass, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("UserName :", userName);
+    console.log("Email :", email);
+    console.log("Password :", password);
+    console.log("Confirm Password :", confPass);
+  };
+
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-5">
-          <div className="card shadow">
-            <div className="card-body">
-              <h2 className="text-center text-primary mb-4">Register</h2>
+    <div className="d-flex justify-content-center">
+      <div className="bg-light border rounded shadow mt-5 w-50 p-5">
+        <h1 className="fw-bold text-center text-primary">Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Full Name</label>
 
-              <div className="mb-3">
-                <label className="form-label">Full Name</label>
-
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Full Name"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Email Address</label>
-
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter Email"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Password</label>
-
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Enter Password"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Confirm Password</label>
-
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Confirm Password"
-                />
-              </div>
-
-              <button className="btn btn-primary w-100">Register</button>
-
-              <p className="text-center mt-3 mb-0">
-                Already have an account? Login
-              </p>
-            </div>
+            <input
+              type="text"
+              className="form-control shadow-none border-2"
+              placeholder="Enter Full Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
           </div>
-        </div>
+          <div className="mb-3">
+            <label className="form-label">Email Address</label>
+
+            <input
+              type="email"
+              className="form-control shadow-none border-2"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+
+            <input
+              type="password" 
+              className="form-control shadow-none border-2"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Confirm Password</label>
+
+            <input
+              type="password"
+              className="form-control shadow-none border-2"
+              placeholder="Confirm Password"
+              value={confPass}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="d-flex justify-content-center mt-3">
+            <button className="btn btn-primary px-4 py-1" type="submit">
+              Register
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
