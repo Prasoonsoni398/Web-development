112dc5bb8d34f30d847fb9508fb414cad1d42e97
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-08 20:09:28 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/my_first_react_app/src/components/Header.jsx
+++ Frontend/my_first_react_app/src/components/Header.jsx
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
+import circleLogo from "../assets/images/circleLogo-DpCri5UD.png";
 function Header() {
   return (
     <>
-      <div className="bg-primary-subtle p-2 d-flex justify-content-between align-items-center">
-        <div className="fs-4 fw-bold text-primary">Prasoon</div>
-        <div className="d-flex gap-4">
-        <Link className="navLinks text-decoration-none text-dark" to="/" >HOME</Link>
-        <Link className="navLinks text-decoration-none text-dark" to="/about">ABOUT</Link>
-        <Link className="navLinks text-decoration-none text-dark" to="/product">PRODUCT</Link>
-        <Link className="navLinks text-decoration-none text-dark" to="/contact">CONTACT</Link>      
-
+      <header>
+        <div class="header d-flex justify-content-between align-items-center mx-5">
+          <div class="headerLogo">
+            <Link to="/">
+              <img
+                src={circleLogo}
+                alt="Logo"
+                class="img-fluid"
+              />
+            </Link>
+          </div>
+          <div class="loginRegister d-flex gap-3">
+            <Link to="./login" class="login btn text-white" id="login">
+              Login
+            </Link>
+            <Link to="./register" class="register btn" id="register">
+              Register
+            </Link>
+          </div>
         </div>
-        <div className="d-flex gap-3">
-          <Link className="btn btn-outline-primary" to={"/login"}>LOGIN</Link>
-          <Link className="btn btn-primary" to={"/register"}>REGISTER</Link>
-        </div>
-      </div>
-    </> 
+      </header>
+    </>
   );
 }
 
--- Frontend/my_first_react_app/src/pages/Login.jsx
+++ Frontend/my_first_react_app/src/pages/Login.jsx
@@ -1,62 +1,113 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import loginBg from "../assets/images/foodTable.webp";
 
 function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("UserName :", userName);
-    console.log("Password :", password);
+    console.log("UserName:", userName);
+    console.log("Password:", password);
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div className="bg-light shadow rounded w-50 mt-5 p-5 border">
-        <h1 className="text-center text-primary fw-bold mb-3">Welcome Back</h1>
+    <div className="position-relative login w-100 ">
+      {/* Background Image */}
+      <div className="login-bg">
+        <img
+          src={loginBg}
+          alt="Food Table"
+          
+        />
+      </div>
+
+      {/* Login Card */}
+      <div className="login-div card d-grid position-absolute p-4">
+        <h2 className="text-center fw-bolder">Welcome Back</h2>
+        <p className="text-center">Login to your Cravings account</p>
+
         <form onSubmit={handleSubmit}>
-          <div className="d-grid gap-1">
-            <label htmlFor="userName" className="fw-semibold">
-              Username
-            </label>
+          {/* Email */}
+          <div className="login-input d-grid gap-2 mb-3 mt-2">
+            <span className="fw-semibold">Email</span>
             <input
-              type="text"
-              id="userName"
-              className="form-control shadow-none border-2"
+              type="email"
+              className="input-field form-control shadow-none"
+              placeholder="Enter your Email"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
+              required
             />
           </div>
-          <div className="d-grid gap-1 mt-3">
-            <label htmlFor="password" className="fw-semibold">
-              Password
-            </label>
-            <input
-              type="text"
-              id="password"
-              className="form-control shadow-none border-2"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+
+          {/* Password */}
+          <div className="login-input d-grid gap-2 mb-3">
+            <span className="fw-semibold">Password</span>
+
+            <div className="d-flex align-items-center pe-3 input-field border rounded">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="form-control shadow-none bg-transparent border-0"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                style={{ cursor: "pointer" }}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
           </div>
-          <div className="d-flex justify-content-center mt-3">
-            <button type="submit" className="btn btn-primary float-end mt-3">
-              Login
-            </button>
+
+          {/* Remember Me */}
+          <div className="remember d-flex justify-content-between align-items-center">
+            <div className="d-flex gap-2 align-items-center">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remember" className="terms">
+                Remember me
+              </label>
+            </div>
+
+            <Link
+              to="/forgot-password"
+              className="terms text-decoration-none"
+            >
+              Forgot Password?
+            </Link>
           </div>
-          <div className="d-flex mt-3 gap-2">
-            <hr className="flex flex-grow-1" />
-            <p>
-              Don't have an account?{" "}
-              <Link to="/register" className="text-decoration-none">
-                Register here
-              </Link>
-            </p>
-            <hr className="flex flex-grow-1" />
-          </div>
+
+          {/* Login Button */}
+          <button
+            className="w-100 mt-4 mb-3 py-2 border-0 rounded-2 text-white"
+            type="submit"
+          >
+            Login
+          </button>
         </form>
+
+        {/* Register Link */}
+        <div className="d-flex align-items-center mb-3 line terms">
+          <hr className="flex-grow-1" />
+          <span className="mx-2">Don't have an account?</span>
+          <hr className="flex-grow-1" />
+        </div>
+
+        <Link
+          to="/register"
+          className="text-center text-decoration-none"
+          id="anchor-underline"
+        >
+          <b>Create an account</b>
+        </Link>
       </div>
     </div>
   );
