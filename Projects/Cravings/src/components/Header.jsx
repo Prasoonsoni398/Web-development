d0b396a989428f4230f50492a7608365ec90dcd2
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-11 21:22:10 +0530

    feat: add initial project setup with React, Vite, and Tailwind CSS
    - Created main entry point (main.jsx) and imported global styles (index.css)
    - Added Header and Footer components
    - Included SVG assets for React and Vite logos
    - Configured Vite with React and Tailwind CSS plugins

---
+++ Projects/Cravings/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeaderLogo from "../assets/images/HeaderLogo.png"
+
+
+const Header = () => {
+  return (
+    <>
+    <div className='h-16 bg-amber-700 flex justify-between px-5 py-2'>
+        <div>
+          <img src={} alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
