c140299bd02d5caf091b1bff1d2b4bf626c54ec9
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-12 20:44:59 +0530

    feat: implement product display grid with details and add to cart button

--- Projects/FakeStore/src/pages/Product.jsx
+++ Projects/FakeStore/src/pages/Product.jsx
@@ -72,7 +72,52 @@ const Product = () => {
         </div>
       )}
 
-     
+      <div className="p-10 grid-cols-1 grid gap-y-9">
+        {products.length > 0 &&
+          products.map((product) => (
+            <div
+              className="flex rounded-2xl border h-60 w-ful shadow-xl"
+              key={product.id}
+            >
+              <div className="w-80 p-4 rounded-s-2xl bg-gray-100 overflow-hidden">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="w-full h-full object-contain hover:scale-110 transition-transform duration-500"
+                />
+              </div>
+
+              <div className="ps-10 p-3 flex flex-col gap-5">
+                <h3 className="text-justify text-lg font-semibold">
+                  {product.title.length > 50
+                    ? product.title.slice(0, 47) + "..."
+                    : product.title}
+                </h3>
+
+                <p className="text-sm text-justify">
+                  {product.description.length > 100
+                    ? product.description.slice(0, 100) + "..."
+                    : product.description}
+                </p>
+
+                <div className="flex gap-2 items-center justify-between">
+                  <h5 className="font-bold">Price: {product.price}</h5>
+                </div>
+
+                <div className="flex gap-2 items-center">
+                  <p>{product.rating?.rate}</p>
+                  <FaStar className="text-yellow-400" />
+                </div>
+
+                <div>
+                  <button className="rounded-full bg-yellow-400 w-50 p-2 hover:bg-yellow-600 hover:scale-102 transition-transform duration-300">
+                    Add To Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </>
   );
 };
