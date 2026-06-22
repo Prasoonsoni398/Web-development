2ff2242f77ab2594d487740137972e3454dd1d00
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-22 20:00:12 +0530

    feat: implement authentication and public routes with controllers

---
+++ Backend/firstbackend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const LoginUser = (req, res) => {
+  res.json({ message: "Login Successfull from auth controller" });
+};
+export const LogoutUser = (req, res) => {
+  res.json({ message: "Logout Successfull from auth controller" });
+};
+export const RegisterUser = (req, res) => {
+  res.json({ message: "Register Successfull from auth controller" });
+};
