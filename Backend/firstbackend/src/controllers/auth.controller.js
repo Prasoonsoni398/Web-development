f30717040e720682f0b97e3921a327cf8d387b16
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-23 21:10:58 +0530

    feat: implement user model and enhance login validation

--- Backend/firstbackend/src/controllers/auth.controller.js
+++ Backend/firstbackend/src/controllers/auth.controller.js
@@ -1,6 +1,22 @@
-export const LoginUser = (req, res) => {
-  res.json({ message: "Login Successfull from auth controller" });
+import User from "../models/user.model.js";
+
+export const LoginUser = async (req, res) => {
+  try {
+    const { fullName, email, password, phone, gender, dob } =
+      req.body;
+
+    if (!fullName || !email || !password || !phone || !gender || !dob) {
+      res.status(400).json({ message: "All fields required" });
+      return;
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "Email Already Registered" });
+      return;
+    }
+  } catch (error) {}
 };
+
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Successfull from auth controller" });
 };
