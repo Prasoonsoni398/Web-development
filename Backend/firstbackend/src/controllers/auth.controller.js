48ac2e5e8087d606732993169b552f489b8cc4db
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-25 20:26:36 +0530

    feat: implement user registration logic and improve error handling in auth.controller.js

--- Backend/firstbackend/src/controllers/auth.controller.js
+++ Backend/firstbackend/src/controllers/auth.controller.js
@@ -1,9 +1,8 @@
 import User from "../models/user.model.js";
 
-export const LoginUser = async (req, res) => {
+export const RegisterUser = async (req, res) => {
   try {
-    const { fullName, email, password, phone, gender, dob } =
-      req.body;
+    const { fullName, email, password, phone, gender, dob } = req.body;
 
     if (!fullName || !email || !password || !phone || !gender || !dob) {
       res.status(400).json({ message: "All fields required" });
@@ -14,13 +13,34 @@ export const LoginUser = async (req, res) => {
       res.status(409).json({ message: "Email Already Registered" });
       return;
     }
-  } catch (error) {}
+
+    const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+
+    const photo = {
+      url: photoUrl,
+      publicId: null,
+    };
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      phone,
+      gender,
+      dob,
+      photo,
+    });
+    res.status(201).json({message:"User Registered Successfully"})
+
+  } catch (error) {
+    res.status(500).json({message:"Internal Sever Error"})
+  }
+};
+
+export const LoginUser = (req, res) => {
+  res.json({ message: "Login Successfull from auth controller" });
 };
 
 
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Successfull from auth controller" });
-};
-export const RegisterUser = (req, res) => {
-  res.json({ message: "Register Successfull from auth controller" });
 };
